1c86689916ad58e43b9dd1b930d4d62c61958326
Celinaki <celina1999@live.se>
2023-07-23 13:58:57 +0200

    first commit

---
+++ server.ts
@@ -0,0 +1,44 @@
+import express, { json, Request, Response } from 'express'
+import { connect } from 'mongoose';
+import recipeRouter from './routes/recipe';
+import categoryRouter from './routes/category';
+
+require('dotenv').config()
+connect(`${process.env.MONGOLAB_URI}`)
+
+
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+
+app.use(json());
+// const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.get('/',(req: Request, res: Response)=>{
+    res.send('HEJSAN')
+})
+
+app.listen(port, () => {
+    console.log(`Example app lyssnar på port ${port}`)
+})
+
+
+app.use('/recipes',recipeRouter)
+
+app.use('/category', categoryRouter)
+
+
+
+
+
+
+
+
+
+
+
+
+
