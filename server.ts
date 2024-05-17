ae2f1e54b7e0da6184ca6feabfee340b1d87a17b
Celinaki <celina1999@live.se>
2024-05-17 11:28:05 +0200

    -

--- server.ts
+++ server.ts
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use(json());
 // const port = 3000;
-const port = process.env.PORT || 3000;
+const port =  3000;
 
 app.get('/',(req: Request, res: Response)=>{
     res.send('HEJSAN')
